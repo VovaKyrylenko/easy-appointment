f1b853e5285a852eafc029502d1c7d43b65e5e7d
Volodymyr Kyrylenko <vovichkyry@gmail.com>
2024-09-04 17:59:31 +0300

    Created common header

---
+++ app/components/common/header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className="bg-blue-700 text-white py-4 mb-6 rounded-lg shadow-lg rounded-t-none">
+      <div className="container mx-auto text-center text-3xl font-extrabold">
+        Apartment Booking
+      </div>
+    </header>
+  );
+};
