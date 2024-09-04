4561344d7b399cbc1b42672a182098d2e70adbd3
Volodymyr Kyrylenko <vovichkyry@gmail.com>
2024-09-04 17:56:57 +0300

    Created custom server with Express

---
+++ app/server.ts
@@ -0,0 +1,36 @@
+import { createRequestHandler } from "@remix-run/express";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs } from "./graphql/schema";
+import { resolvers } from "./graphql/resolvers";
+
+const viteDevServer =
+  process.env.NODE_ENV === "production"
+    ? null
+    : await import("vite").then((vite) =>
+        vite.createServer({
+          server: { middlewareMode: true },
+        })
+      );
+
+const app = express();
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+await apolloServer.start();
+apolloServer.applyMiddleware({ app });
+
+app.use(
+  viteDevServer ? viteDevServer.middlewares : express.static("build/client")
+);
+
+const build = viteDevServer
+  ? () => viteDevServer.ssrLoadModule("virtual:remix/server-build")
+  : //   @ts-ignore import error
+    await import("./build/server/index.js");
+
+app.all("*", createRequestHandler({ build }));
+
+app.listen(3000, () => {
+  console.log("App listening on http://localhost:3000");
+  console.log(`GraphQL server ready at http://localhost:3000/graphql`);
+});
