b5ad0fe41319ce800a202d8ecd723733a98a9753
Volodymyr Kyrylenko <vovichkyry@gmail.com>
2024-09-06 09:58:30 +0300

    Updated header with button to redirect user to dashboard/login page

--- app/components/common/header.tsx
+++ app/components/common/header.tsx
@@ -1,8 +1,20 @@
-export const Header = () => {
+import { Button } from "~/components/ui/button";
+import { Link } from "@remix-run/react";
+
+interface HeaderProps {
+  isAuthenticated: boolean;
+}
+
+export const Header = ({ isAuthenticated }: HeaderProps) => {
   return (
     <header className="bg-blue-700 text-white py-4 mb-6 rounded-lg shadow-lg rounded-t-none">
-      <div className="container mx-auto text-center text-3xl font-extrabold">
-        Apartment Booking
+      <div className="container mx-auto text-center text-3xl font-extrabold flex justify-between items-center">
+        <span>Apartment Booking</span>
+        <Button asChild className="bg-white text-blue-700 hover:bg-blue-100">
+          <Link to={isAuthenticated ? "/dashboard" : "/login"}>
+            {isAuthenticated ? "Dashboard" : "Login"}
+          </Link>
+        </Button>
       </div>
     </header>
   );
--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -1,10 +1,13 @@
-import { Outlet } from "@remix-run/react";
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { Outlet, useLoaderData } from "@remix-run/react";
 import { Header } from "~/components/common/header";
+import { authenticator } from "~/services/auth.server";
 
 export default function AppPage() {
+    const { isAuthenticated } = useLoaderData<typeof loader>() || false;
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col">
-      <Header />
+      <Header isAuthenticated={isAuthenticated} />
       <main className="container mx-auto flex-1 p-6 mb-6 bg-white rounded-lg shadow-md flex flex-col">
         <Outlet />
       </main>
@@ -12,3 +15,7 @@ export default function AppPage() {
   );
 }
 
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const user = await authenticator.isAuthenticated(request);
+  return json({ isAuthenticated: !!user });
+};
